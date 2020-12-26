797845b3e8676a4552de7c003e63d2582b394e0e
Zhenyu Sha <zhenyusha@didiglobal.com>
2020-12-26 10:51:30 -0800

    successfully publish bag

--- charter1/sync_msg.py
+++ charter1/sync_msg.py
@@ -1,21 +1,39 @@
-
 import rospy
+import rosbag
 import message_filters
 from sensor_msgs.msg import Image, PointCloud2
 
-_input_lidar_topic ="/rslidar_points"
+_input_lidar_topic = "/rslidar_points"
 _input_image_topic = "/zed/zed_node/left/image_rect_color"
 
-_output_lidar_topic= "/zhenyusha_point_cloud"
+_output_lidar_topic = "/zhenyusha_point_cloud"
 _output_image_topic = "/zhenyusha_img"
+#output bag file
+_bag = None
+
 
 def callback(input_image, input_point_cloud):
-    rospy.loginfo("incallback")
-    
+    _bag.write(topic=_output_lidar_topic,
+               msg=input_point_cloud,
+               t=input_point_cloud.header.stamp)
+    _bag.write(topic=_output_image_topic,
+               msg=input_image,
+               t=input_point_cloud.header.stamp)
+
+
 if __name__ == '__main__':
-   rospy.init_node('sync_msg_time', anonymous=False) 
-   image_sub = message_filters.Subscriber(_input_image_topic, Image)
-   point_cloud_sub = message_filters.Subscriber(_input_lidar_topic, PointCloud2) 
-   ts = message_filters.TimeSynchronizer([image_sub, point_cloud_sub], 10)
-   ts.registerCallback(callback)
-   rospy.spin()
+    try:
+        rospy.init_node('sync_msg_time', anonymous=False)
+        #TODO file name args
+        _bag = rosbag.Bag('charpter1.bag', 'w')
+
+        image_sub = message_filters.Subscriber(_input_image_topic, Image)
+        point_cloud_sub = message_filters.Subscriber(_input_lidar_topic,
+                                                     PointCloud2)
+
+        ts = message_filters.ApproximateTimeSynchronizer(
+            [image_sub, point_cloud_sub], 10, 0.1, allow_headerless=False)
+        ts.registerCallback(cb=callback)
+        rospy.spin()
+    finally:
+        _bag.close()
