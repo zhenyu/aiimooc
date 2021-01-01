b4a10fa8575bdb5ce7288076f931cf73f770786d
Zhenyu Sha <zhenyusha@didiglobal.com>
2021-01-01 09:54:39 -0800

    typo and reorg

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # aiimooc
+Homework for https://college.aiimooc.com/preview/651
---
+++ chapter2/problem1/zhenyusha_aiimooc_chapter2_problem1/src/main.cpp
@@ -0,0 +1,8 @@
+#include <ros/ros.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+void main(){
+    return 0;
+}
