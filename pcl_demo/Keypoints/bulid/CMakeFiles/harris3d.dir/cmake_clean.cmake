654e7ebbd137887c146ba695c570835a9f140773
Zhenyu Sha <zhenyusha@Zhenyus-MacBook-Pro.local>
2021-02-06 11:22:47 +0800

    c++ 14

---
+++ pcl_demo/Keypoints/bulid/CMakeFiles/harris3d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/harris3d.dir/harris3d_keypoint_extraction.cpp.o"
+  "harris3d"
+  "harris3d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/harris3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pcl_demo/Keypoints/bulid/CMakeFiles/narf_kp_extr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/narf_kp_extr.dir/narf_keypoint_extraction.cpp.o"
+  "narf_kp_extr"
+  "narf_kp_extr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/narf_kp_extr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
